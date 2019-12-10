fd036f09cbe659aad97be4b5cd9dcd177e706c3f
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-12-11 01:37:36 +0900

    update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="nvidia-htop",
-    version="1.0",
+    name="nvhtop",
+    version="0.1",
     packages=find_packages(),
     description="A tool for enriching the output of nvidia-smi",
     long_description=long_description,

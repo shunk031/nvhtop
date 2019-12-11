8747b4ecd15ba84920a6353cff961b5f37d8ce24
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-12-11 13:06:51 +0900

    update

--- setup.py
+++ setup.py
@@ -9,8 +9,17 @@ setup(
     name="nvhtop",
     version="0.1",
     packages=find_packages(),
+    author="Shunsuke Kitada",
+    author_email="shunsuke.kitada.0831@gmail.com",
     description="A tool for enriching the output of nvidia-smi",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["termcolor"],
     entry_points={"console_scripts": ["nvhtop=nvhtop.run:run"]},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )

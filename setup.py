2fc187b89d0dd4b572d01d63f370bc7388cf310e
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-12-11 13:19:39 +0900

    update

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     description="A tool for enriching the output of nvidia-smi",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/shunk031/nvhtop",
     install_requires=["termcolor"],
     entry_points={"console_scripts": ["nvhtop=nvhtop.run:run"]},
     classifiers=[
